4c1da4776463919b3a79948b0da6c3b74ed5c426
andy <>
2025-11-16 12:30:27 +0800

    fix

---
+++ notefile/算法/listnode/listnode.go
@@ -0,0 +1,33 @@
+package listnode
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func main() {
+
+}
+
+//1、删除有序列表中重复的元素
+//给出的链表为  1→1→2,返回 1→2
+func deleteDup(head *ListNode) *ListNode {
+	if head == nil {
+		return head
+	}
+
+	//双指针
+	slow, fast := head, head.Next
+	for fast != nil {
+		//快慢指针相同，则慢指针指向快指针
+		if slow.Val == fast.Val {
+			slow.Next = fast.Next
+		} else {
+			//否则向后滑动
+			slow = slow.Next
+		}
+		fast = fast.Next
+	}
+	
+	return head
+}
--- notefile/算法/sort/sortA.go
+++ notefile/算法/sort/sortA.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func main() {
 	fmt.Println(LeastNum([]int{4, 5, 1, 6, 2, 7, 3, 8}, 4))
+	fmt.Println(bubbleSort([]int{4, 5, 1, 6, 2, 7, 3, 8}))
 }
 
 //给定一个长度为n的可能重复的数组，找出其中不去重复的最小k个数
@@ -18,6 +19,7 @@ func LeastNum(input []int, k int) []int {
 	return newNum[:k]
 }
 
+// 快速排序
 func quickSort(input []int) []int {
 	if len(input) < 2 {
 		return input
@@ -37,4 +39,19 @@ func quickSort(input []int) []int {
 	//input = append(quickSort(left), pivot)
 	//input = append(input, quickSort(right)...)
 	return append(append(quickSort(left), pivot), quickSort(right)...)
+}
+
+// 冒泡排序 5,3,2,4
+// 第一次遍历将最大元素冒泡到最后 3，2，4，5，第二次遍历将最大元素冒泡到最后 2，3，4，5
+func bubbleSort(input []int) []int {
+	for i := 0; i < len(input)-1; i++ {
+		//每次遍历将最大元素冒泡到最后
+		for j := 0; j < len(input)-i-1; j++ {
+			if input[j] > input[j+1] {
+				//交换元素
+				input[j], input[j+1] = input[j+1], input[j]
+			}
+		}
+	}
+	return input
 }
